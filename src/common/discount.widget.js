5b5170f8f34d71d74a02852dd710e9f320f33ba0
sharitu <ritu@salessimplify.com>
2023-07-30 23:03:25 +0530

    Added test variants

--- src/common/discount.widget.js
+++ src/common/discount.widget.js
@@ -5,31 +5,31 @@
 const priceExtractionRegex = /^(\D+)(\d+(\.\d{1,2})?)$/;
 
 const getDiscount = (price) => {
-  //The minimum cost of the item to receive a discount is $20.
+  // The minimum cost of the item to receive a discount is $20.
   if (price < 20)
     return {
       percent: 0,
-      discountedPrice: price,
+      discountedPrice: Math.round(((price) + Number.EPSILON) * 100) / 100,
     };
 
   // 20% discount if the cost of the item is less than $100.
   if (price < 100)
     return {
       percent: 20,
-      discountedPrice: price * 0.8,
+      discountedPrice: Math.round(((price * 0.8) + Number.EPSILON) * 100) / 100,
     };
 
   // 30% discount if the cost of the item is between $100 and $499.99.
   if (price < 500)
     return {
       percent: 30,
-      discountedPrice: price * 0.7,
+      discountedPrice: Math.round(((price * 0.7) + Number.EPSILON) * 100) / 100,
     };
 
   // 40% discount if the cost of the item is $500 or more.
   return {
     percent: 40,
-    discountedPrice: price * 0.6,
+    discountedPrice: Math.round(((price * 0.6) + Number.EPSILON) * 100) / 100,
   };
 };
 
