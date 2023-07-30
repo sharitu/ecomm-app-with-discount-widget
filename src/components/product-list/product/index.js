4bd78820f787aa50ca70741cf56a5becf18a0641
sharitu <ritu@salessimplify.com>
2023-07-30 23:09:41 +0530

    Refactored: null check for price

--- src/components/product-list/product/index.js
+++ src/components/product-list/product/index.js
@@ -1,6 +1,8 @@
 export default function Product ({details = {}}) {
   const {id, name, description, price, discount, imgUrl} = details;
 
+  const _isNull = (val) => (val === null || typeof val === 'undefined')
+
   return (<div key={id} className="flex flex-col justify-start items-center p-5 mb-5">
     <div className="relative h-80 w-44">
       <img
@@ -16,7 +18,7 @@ export default function Product ({details = {}}) {
     </div>
     <h3 className="text-xl md:text-2xl font-bold mt-4 md:mt-12">{name}</h3>
     <p className="mt-3">{description}</p>
-    <p className={`price-${id} text-xl md:text-2xl mt-3 ${discount?.percent > 0 ? 'line-through' :'' }`}>{price?.code}{price?.value}</p>
+    { !_isNull(price.value) && price?.value >= 0 && <p className={`price-${id} text-xl md:text-2xl mt-3 ${discount?.percent > 0 ? 'line-through' :'' }`}>{price?.code}{price?.value}</p>}
     {discount?.percent > 0 && <p className="text-xl md:text-2xl">{discount?.code}{discount?.discountedPrice} ({discount?.percent}%)</p>}
   </div>);
 }
